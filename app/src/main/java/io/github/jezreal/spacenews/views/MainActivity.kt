1a10e3853e86d0b24006f6250f1e2f494d3c8a28
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-03-20 18:23:25 +0800

    setup navigation

--- app/src/main/java/io/github/jezreal/spacenews/views/MainActivity.kt
+++ app/src/main/java/io/github/jezreal/spacenews/views/MainActivity.kt
@@ -2,13 +2,28 @@ package io.github.jezreal.spacenews.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.jezreal.spacenews.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
