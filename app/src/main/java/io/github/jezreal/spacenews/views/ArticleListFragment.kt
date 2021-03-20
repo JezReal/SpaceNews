36ee6e5cf9393409c4e487657e71f212566e4e5f
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-03-20 18:36:10 +0800

    implement loading articles

--- app/src/main/java/io/github/jezreal/spacenews/views/ArticleListFragment.kt
+++ app/src/main/java/io/github/jezreal/spacenews/views/ArticleListFragment.kt
@@ -1,16 +1,26 @@
 package io.github.jezreal.spacenews.views
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.jezreal.spacenews.recyclerview.ArticleAdapter
 import io.github.jezreal.spacenews.databinding.FragmentArticleListBinding
+import io.github.jezreal.spacenews.models.Article
+import io.github.jezreal.spacenews.recyclerview.ArticleAdapter
 import io.github.jezreal.spacenews.viewmodels.ArticleViewModel
-import io.github.jezreal.spacenews.viewmodels.ArticleViewModel.*
+import io.github.jezreal.spacenews.viewmodels.ArticleViewModel.ArticleEvent.LoadUrl
+import io.github.jezreal.spacenews.viewmodels.ArticleViewModel.ArticleEvent.ShowSnackBar
+import io.github.jezreal.spacenews.viewmodels.ArticleViewModel.ArticleState.*
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class ArticleListFragment : Fragment() {
@@ -18,6 +28,7 @@ class ArticleListFragment : Fragment() {
     private lateinit var binding: FragmentArticleListBinding
     private val viewModel: ArticleViewModel by viewModels()
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,26 +38,46 @@ class ArticleListFragment : Fragment() {
 
         viewModel.articleList.observe(viewLifecycleOwner) { articleState ->
             when (articleState) {
-                is ArticleState.Empty -> {
+                is Empty -> {
                     binding.recyclerView.visibility = View.GONE
                     binding.errorMessage.visibility = View.GONE
                     binding.loading.visibility = View.GONE
                 }
-                is ArticleState.Loading -> {
+                is Loading -> {
                     binding.loading.visibility = View.VISIBLE
                 }
-                is ArticleState.Sucess -> {
+                is Success -> {
                     binding.loading.visibility = View.GONE
                     binding.recyclerView.visibility = View.VISIBLE
-                    val adapter = ArticleAdapter()
+                    val adapter = ArticleAdapter { article ->
+                        adapterOnClick(article)
+                    }
                     binding.recyclerView.adapter = adapter
                     adapter.submitList(articleState.articles)
                 }
 
-                is ArticleState.Error -> {
+                is Error -> {
                     binding.loading.visibility = View.GONE
                     binding.errorMessage.visibility = View.VISIBLE
                     binding.errorMessage.text = articleState.message
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.articleEvent.collect { event ->
+                when (event) {
+                    is ShowSnackBar -> {
+                        Snackbar.make(binding.root, event.message, Snackbar.LENGTH_LONG).show()
+                        Log.d("ArticleListFragment", "vent collected")
+                    }
+                    is LoadUrl -> {
+                        findNavController().navigate(
+                            ArticleListFragmentDirections.actionArticleListFragmentToArticleFragment(
+                                event.url
+                            )
+                        )
+                    }
                 }
             }
         }
@@ -56,5 +87,8 @@ class ArticleListFragment : Fragment() {
         return binding.root
     }
 
+    private fun adapterOnClick(article: Article) {
+        viewModel.loadUrl(article.url)
+    }
 
 }
