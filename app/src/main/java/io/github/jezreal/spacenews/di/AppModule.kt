e99c199e6bae6ecd40c4e8e797dbc84a053df32b
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-20 14:57:10 +0800

    create room database dependency for di

--- app/src/main/java/io/github/jezreal/spacenews/di/AppModule.kt
+++ app/src/main/java/io/github/jezreal/spacenews/di/AppModule.kt
@@ -1,9 +1,14 @@
 package io.github.jezreal.spacenews.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.github.jezreal.spacenews.database.ArticleDao
+import io.github.jezreal.spacenews.database.ArticleDatabase
 import io.github.jezreal.spacenews.network.SpaceflightNewsApi
 import io.github.jezreal.spacenews.repository.ArticleRepository
 import retrofit2.Retrofit
@@ -23,6 +28,23 @@ object AppModule {
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
             .create(SpaceflightNewsApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideArticleDatabase(@ApplicationContext context: Context): ArticleDatabase {
+        return Room.databaseBuilder(
+            context,
+            ArticleDatabase::class.java,
+            "article_database"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun provideArticleDao(database: ArticleDatabase): ArticleDao {
+        return database.articleDao
     }
 
     @Singleton
